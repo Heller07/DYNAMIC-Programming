b606cc5ae3e7c4c7ee139bb608c3c2447ed93dbd
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-02-24 14:00:48 +0530

    Update dp_intro._fibo.cpp

--- dp_intro._fibo.cpp
+++ dp_intro._fibo.cpp
@@ -1,15 +1,6 @@
 #include<bits/stdc++.h>
 using namespace std;
-
-int fib(int n, vector<int>&dp){
-        if(n<=1)return n;
-
-        if(dp[n]!=-1) return dp[n];
-
-        dp[n] = fib(n-1, dp) + fib(n-2, dp);
-        return dp[n];
-}
-
+//BOTTOM UP approach
 int main()
 {
         /*
@@ -31,4 +22,33 @@ int main()
 
 
 
+}
+
+
+//TOP DOWN approach
+#include<bits/stdc++.h>
+using namespace std;
+int fib(int n, vector<int> &dp){
+//     base case
+    if(n <= 1){
+        return n;
+    }
+    
+    if(dp[n] != -1){
+        return dp[n];
+    }
+    dp[n] = fib(n-1, dp) + fib(n-2, dp);
+    return dp[n];
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    
+    vector<int> dp(n+1);
+    for(int i = 0; i<=n; i++){
+        dp[i] = -1;
+    }
+    cout<<fib(n, dp)<<endl;
 }
