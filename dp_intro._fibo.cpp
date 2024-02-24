a5dcaa05b48e8f0f6353a2fe76dacc2b6074ed4d
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-02-24 14:07:10 +0530

    Update dp_intro._fibo.cpp

--- dp_intro._fibo.cpp
+++ dp_intro._fibo.cpp
@@ -52,3 +52,34 @@ int main()
     }
     cout<<fib(n, dp)<<endl;
 }
+
+
+//SPACE OPTIMISATION
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main()
+{
+        /*
+        *  Write your code here. 
+         *  Read input as specified in the question.
+         *  Print output as specified in the question.
+        */
+        int n;
+        cin>>n;
+        int prev1 = 1;
+        int prev2 = 0;
+        
+        if(n==0){
+                return prev2;
+        }
+        for(int i=2;i<=n;i++){
+                int curr = prev1 + prev2;
+                prev2 = prev1;
+                prev1 = curr;
+        }  
+        cout<<prev1<<endl;
+        //return dp[n+1];
+}
